127ed1aacb5a1479d9e3e6407f11cf1e16b88fc7
Martin Laureta <martin.laureta@live.com>
2020-06-14 13:06:14 +0800

    Visual #1

--- GoldfishVisuals/GoldfishVisuals.py
+++ GoldfishVisuals/GoldfishVisuals.py
@@ -6,11 +6,13 @@ import json as js
 import collections
 
 color = {"W":"White", "U":"Blue", "B":"Black", "R":"Red", "G":"Green", "C":"Colorless"}
-dual = [["W","U"],["W","B"],["U","B"],["U","R"],["B","R"],["B","G"],["R","G"],["R","W"],["G","W"],["G","U"]]
-tri = [["G","W","U"],["W","U","B"],["U","B","R"],["B","R","G"],["R","G","W"],["W","B","G"],["U","R","W"],["B","G","U"],["R","W","B"]]
-quad = [["W","U","B","R"],["U","B","R","G"],["B","R","G","W"],["R","G","W","U"],["G","W","U","B"]]
-wubrg = ['W','U','B','R','G']
-order = ["W","U","B","R","G","C","WU","WB","UB","UR","BR","BG","RG","RW","GW","GU","GWU","WUB","UBR","BRG","RGW","WBG","URW","BGU","RWB","GUR","WUBR","UBRG","BRGW","RGWU","GWUB","WUBRG"]
+dual = (["W","U"],["W","B"],["U","B"],["U","R"],["B","R"],["B","G"],["R","G"],["R","W"],["G","W"],["G","U"])
+tri = (["G","W","U"],["W","U","B"],["U","B","R"],["B","R","G"],["R","G","W"],["W","B","G"],["U","R","W"],["B","G","U"],["R","W","B"])
+quad = (["W","U","B","R"],["U","B","R","G"],["B","R","G","W"],["R","G","W","U"],["G","W","U","B"])
+wubrg = ('W','U','B','R','G')
+order = ("W","U","B","R","G","C","WU","WB","UB","UR","BR","BG","RG","RW","GW","GU","GWU","WUB","UBR","BRG","RGW","WBG","URW","BGU","RWB","GUR","WUBR","UBRG","BRGW","RGWU","GWUB","WUBRG")
+
+### GET DATA FROM GOOGLE SHEETS
 data = pa.DataFrame(pa.read_csv("https://docs.google.com/spreadsheets/d/1nqQ01wzCNwJJOpVjLtOpPuOhm34CMwrZ1pImoZNOMhA/export?format=csv&gid=1802173994"))
 
 rank = data[["Rank"]]
@@ -28,10 +30,10 @@ def pullAPIData():
         conn.request("GET", ("/cards/named?exact={}".format(n).replace(" ", "+")))
         resp = js.loads(conn.getresponse().read())
         
-        # Create CMC column
+        ### Create CMC (Converted Mana Cost) column
         cmc.append(int(resp["cmc"]))
 
-        # Create Colors column
+        ### Create Colors column
         if len(resp["colors"]) > 0:
             if len(resp["colors"]) == 2:
                 for sort_order in dual:
@@ -58,7 +60,18 @@ def pullAPIData():
 
 pullAPIData()
 
+### GET AVERAGE CONVERTED MANA COST
 average = data["CMC"].mean()
-count = dict(collections.Counter(data["Colors"]))
-
 print("Average converted mana cost of top cards: ", average)
+
+
+### GET FREQUENCY OF CARDS BY COLOR/S
+count = collections.Counter(data["Colors"])
+count = sorted(count.items())
+count = [tuple for x in order for tuple in count if tuple[0] == x]
+frequency = [i[1] for i in count]
+col = [i[0] for i in count]
+
+### DISPLAY FREQUENCY GRAPH
+plt.bar(col, frequency)
+plt.show()
